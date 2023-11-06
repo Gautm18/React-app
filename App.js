162fd9350b176754124dcfd58370092554fc9888
Gautam <gshandilya44@gmail.com>
2023-11-06 23:27:11 +0530

    adding react as a library and removing CDN links

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
 const firstVar = React.createElement("div", { className: "design" }, "Hello Gautam to React")
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(firstVar)
